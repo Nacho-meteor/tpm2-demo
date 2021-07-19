b4c76d2a0ea6b6b172abea33e20983b40221c892
刘鑫 <liuxin@uniontech.com>
2021-07-19 17:46:30 +0800

    feat: Add esapi layer-based encryption and decryption practice demo
    Log: Add esapi layer-based encryption and decryption practice demo

--- esapi/esapi.c
+++ esapi/esapi.c
@@ -159,6 +159,13 @@ test_invoke_esapi(ESYS_CONTEXT * esys_context) {
     }else{
         goto end;
     }
+
+    ret = test_esys_encrypt_decrypt(esys_context);
+    if (ret == EXIT_SUCCESS){
+        printf("Esys encrypt and decrypt successful!!\n");
+    }else{
+        goto end; 
+    }
 end:      
     return ret;
 }
@@ -411,4 +418,317 @@ error:
     }
 
     return EXIT_FAILURE;
+}
+
+int
+test_esys_encrypt_decrypt(ESYS_CONTEXT * esys_context)
+{
+    TSS2_RC r;
+    ESYS_TR primaryHandle = ESYS_TR_NONE;
+    ESYS_TR loadedKeyHandle = ESYS_TR_NONE;
+    int failure_return = EXIT_FAILURE;
+
+    TPM2B_AUTH authValuePrimary = {
+        .size = 5,
+        .buffer = {1, 2, 3, 4, 5}
+    };
+
+    TPM2B_SENSITIVE_CREATE inSensitivePrimary = {
+        .size = 4,
+        .sensitive = {
+            .userAuth = {
+                .size = 0,
+                .buffer= {0},
+            },
+            .data = {
+                .size = 0,
+                .buffer = {0},
+            },
+        },
+    };
+
+    inSensitivePrimary.sensitive.userAuth = authValuePrimary;
+
+    TPM2B_PUBLIC inPublic = {
+        .size = 0,
+        .publicArea = {
+            .type = TPM2_ALG_RSA,
+            .nameAlg = TPM2_ALG_SHA256,
+            .objectAttributes = (TPMA_OBJECT_USERWITHAUTH |
+                                 TPMA_OBJECT_RESTRICTED |
+                                 TPMA_OBJECT_DECRYPT | 
+                                 TPMA_OBJECT_FIXEDTPM |
+                                 TPMA_OBJECT_FIXEDPARENT |
+                                 TPMA_OBJECT_SENSITIVEDATAORIGIN),
+            .authPolicy = {
+                .size = 0,
+            },
+            .parameters.rsaDetail = {
+                .symmetric = {
+                    .algorithm = TPM2_ALG_AES,
+                    .keyBits.aes = 128,
+                    .mode.aes = TPM2_ALG_CFB
+                },
+                .scheme = {
+                    .scheme = TPM2_ALG_NULL
+                },
+                .keyBits = 2048,
+                .exponent = 0,
+            },
+            .unique.rsa = {
+                .size = 0,
+                .buffer = {},
+            },
+        },
+    };
+
+    TPM2B_DATA outsideInfo = {
+        .size = 0,
+        .buffer = {},
+    };
+
+    TPML_PCR_SELECTION creationPCR = {
+        .count = 0,
+    };
+
+    TPM2B_AUTH authValue = {
+        .size = 0,
+        .buffer = {}
+    };
+
+    r = Esys_TR_SetAuth(esys_context, ESYS_TR_RH_OWNER, &authValue);
+    if ( r != TSS2_RC_SUCCESS ) {
+        LOG_ERROR( "Error: TR_SetAuth:0x%x", r);
+        goto error;
+    }
+
+    TPM2B_PUBLIC *outPublic;
+    TPM2B_CREATION_DATA *creationData;
+    TPM2B_DIGEST *creationHash;
+    TPMT_TK_CREATION *creationTicket;
+
+    r = Esys_CreatePrimary(esys_context,ESYS_TR_RH_OWNER,ESYS_TR_PASSWORD,
+                            ESYS_TR_NONE, ESYS_TR_NONE,
+                            &inSensitivePrimary, &inPublic,
+                            &outsideInfo, &creationPCR, &primaryHandle,
+                            &outPublic, &creationData, &creationHash,
+                            &creationTicket);
+    if ( r != TSS2_RC_SUCCESS ) {
+        LOG_ERROR( "Error: esys create primary:0x%x", r);
+        goto error;
+    }
+
+    r = Esys_TR_SetAuth(esys_context, primaryHandle, &authValuePrimary);
+    if ( r != TSS2_RC_SUCCESS ) {
+        LOG_ERROR( "Error: TR_SetAuth:0x%x", r);
+        goto error;
+    }
+    
+    TPM2B_AUTH authKey2 = {
+        .size = 6,
+        .buffer = {6, 7, 8, 9, 10, 11}
+    };
+
+    TPM2B_SENSITIVE_CREATE inSensitive2 = {
+        .size = 1,
+        .sensitive = {
+            .userAuth = {
+                .size = 0,
+                .buffer = {0}
+            },
+            .data = {
+                .size = 16,
+                .buffer = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 16}
+            }
+        }
+    };
+
+    inSensitive2.sensitive.userAuth  = authKey2;
+
+    TPM2B_PUBLIC inPublic2 = {
+        .size = 0,
+        .publicArea = {
+            .type = TPM2_ALG_SYMCIPHER,
+            .nameAlg = TPM2_ALG_SHA256,
+            .objectAttributes = (TPMA_OBJECT_USERWITHAUTH |
+                                 TPMA_OBJECT_SIGN_ENCRYPT |
+                                 TPMA_OBJECT_DECRYPT),
+
+            .authPolicy = {
+                 .size = 0,
+             },
+            .parameters.symDetail = {
+                 .sym = {
+                     .algorithm = TPM2_ALG_AES,
+                     .keyBits = {.aes = 128},
+                     .mode = {.aes = TPM2_ALG_CFB}}
+             },
+            .unique.sym = {
+                 .size = 0,
+                 .buffer = {}
+             }
+        }
+    };
+
+    TPM2B_DATA outsideInfo2 = {
+        .size = 0,
+        .buffer = {}
+        ,
+    };
+
+    TPML_PCR_SELECTION creationPCR2 = {
+        .count = 0,
+    };
+
+    TPM2B_PUBLIC *outPublic2;
+    TPM2B_PRIVATE *outPrivate2;
+    TPM2B_CREATION_DATA *creationData2;
+    TPM2B_DIGEST *creationHash2;
+    TPMT_TK_CREATION *creationTicket2;
+
+    r = Esys_Create(esys_context,
+                    primaryHandle,
+                    ESYS_TR_PASSWORD, ESYS_TR_NONE, ESYS_TR_NONE,
+                    &inSensitive2,
+                    &inPublic2,
+                    &outsideInfo2,
+                    &creationPCR2,
+                    &outPrivate2,
+                    &outPublic2,
+                    &creationData2, &creationHash2, &creationTicket2);
+    if ( r != TSS2_RC_SUCCESS ) {
+        LOG_ERROR( "Error esys create:0x%x", r);
+        goto error;
+    }
+
+    LOG_INFO("AES key created.");
+
+    r = Esys_Load(esys_context,
+                  primaryHandle,
+                  ESYS_TR_PASSWORD,
+                  ESYS_TR_NONE,
+                  ESYS_TR_NONE, outPrivate2, outPublic2, &loadedKeyHandle);
+    if ( r != TSS2_RC_SUCCESS ) {
+        LOG_ERROR( "Error esys load:0x%x", r);
+        goto error;
+    }
+
+    LOG_INFO("AES key loaded.");
+
+    r = Esys_TR_SetAuth(esys_context, loadedKeyHandle, &authKey2);
+    if ( r != TSS2_RC_SUCCESS ) {
+        LOG_ERROR( "Error esys TR_SetAuth:0x%x", r);
+        goto error;
+    }
+
+    ESYS_TR keyHandle_handle = loadedKeyHandle;
+    TPMI_YES_NO decrypt = TPM2_YES;
+    TPMI_YES_NO encrypt = TPM2_NO;
+    TPMI_ALG_SYM_MODE mode = TPM2_ALG_NULL;
+    TPM2B_IV ivIn = {
+        .size = 16,
+        .buffer = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 16}
+    };
+    TPM2B_MAX_BUFFER inData = {
+        .size = 16,
+        .buffer = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 16}
+    };
+    TPM2B_MAX_BUFFER *outData;
+    TPM2B_IV *ivOut;
+
+    r = Esys_EncryptDecrypt(
+        esys_context,
+        keyHandle_handle,
+        ESYS_TR_PASSWORD,
+        ESYS_TR_NONE,
+        ESYS_TR_NONE,
+        encrypt,
+        mode,
+        &ivIn,
+        &inData,
+        &outData,
+        &ivOut);
+
+    if ((r == TPM2_RC_COMMAND_CODE) ||
+        (r == (TPM2_RC_COMMAND_CODE | TSS2_RESMGR_RC_LAYER)) ||
+        (r == (TPM2_RC_COMMAND_CODE | TSS2_RESMGR_TPM_RC_LAYER))) {
+        LOG_ERROR("Command TPM2_EncryptDecrypt not supported by TPM.");
+        failure_return = EXIT_SKIP;
+        goto error;
+    }
+
+    if ( r != TSS2_RC_SUCCESS ) {
+        LOG_ERROR( "Error EncryptDecrypt:0x%x", r);
+        goto error;
+    }
+
+    TPM2B_MAX_BUFFER *outData2;
+    TPM2B_IV *ivOut2;
+
+    r = Esys_EncryptDecrypt(
+        esys_context,
+        keyHandle_handle,
+        ESYS_TR_PASSWORD,
+        ESYS_TR_NONE,
+        ESYS_TR_NONE,
+        decrypt,
+        mode,
+        &ivIn,
+        outData,
+        &outData2,
+        &ivOut2);
+
+    if ((r == TPM2_RC_COMMAND_CODE) ||
+        (r == (TPM2_RC_COMMAND_CODE | TSS2_RESMGR_RC_LAYER)) ||
+        (r == (TPM2_RC_COMMAND_CODE | TSS2_RESMGR_TPM_RC_LAYER))) {
+        LOG_ERROR("Command TPM2_EncryptDecrypt not supported by TPM.");
+        failure_return = EXIT_SKIP;
+        goto error;
+    }
+
+    if ( r != TSS2_RC_SUCCESS ) {
+        LOG_ERROR( "Error EncryptDecrypt:0x%x", r);
+        goto error;
+    }
+
+    LOGBLOB_DEBUG(&outData2->buffer[0], outData2->size, "** Decrypted data **");
+
+    if (outData2->size != inData.size ||
+        memcmp(&outData2->buffer, &inData.buffer[0], outData2->size) != 0) {
+        LOG_ERROR("Error: decrypted text not  equal to origin");
+        goto error;
+    }
+
+    r = Esys_FlushContext(esys_context, primaryHandle);
+    if ( r != TSS2_RC_SUCCESS ) {
+        LOG_ERROR( "Error during FlushContext:0x%x", r);
+        goto error;
+    }
+    if ( r != TSS2_RC_SUCCESS ) {
+        LOG_ERROR( "Error during FlushContext:0x%x", r);
+        goto error;
+    }
+
+    primaryHandle = ESYS_TR_NONE;
+
+    r = Esys_FlushContext(esys_context, loadedKeyHandle);
+    if ( r != TSS2_RC_SUCCESS ) {
+        LOG_ERROR( "Error during FlushContext:0x%x", r);
+        goto error;
+    }
+
+    return EXIT_SUCCESS;
+error:
+    if (primaryHandle != ESYS_TR_NONE) {
+        if (Esys_FlushContext(esys_context, primaryHandle) != TSS2_RC_SUCCESS) {
+            LOG_ERROR("Cleanup primaryHandle failed.");
+        }
+    }
+
+    if (loadedKeyHandle != ESYS_TR_NONE) {
+        if (Esys_FlushContext(esys_context, loadedKeyHandle) != TSS2_RC_SUCCESS) {
+            LOG_ERROR("Cleanup loadedKeyHandle failed.");
+        }
+    }
+    return failure_return;
 }
